bf7a8447230e396f2742813348b82c723347021d
David Durieux <d.durieux@siprossii.com>
2014-08-21 01:55:16 +0200

    Add times to 'bench'

--- tools/delete.sh
+++ tools/delete.sh
@@ -1,1 +1,1 @@
-curl --request DELETE  http://127.0.0.1/glping/public/computers/1
+curl -s -w \\n"%{time_connect}:%{time_starttransfer}:%{time_total}"\\n --request DELETE  http://127.0.0.1/glping/public/computers/1
--- tools/post.sh
+++ tools/post.sh
@@ -1,1 +1,1 @@
-curl --header "Content-type: application/json" --request POST --data '{"name": "pc xxxx"}' http://127.0.0.1/glping/public/computers
+curl -s -w \\n"%{time_connect}:%{time_starttransfer}:%{time_total}"\\n --header "Content-type: application/json" --request POST --data '{"name": "pc xxxx"}' http://127.0.0.1/glping/public/computers
