808201ed844f154bb3148ad884bfe0e73f2c3a3b
David Durieux <d.durieux@siprossii.com>
2014-08-21 11:40:07 +0200

    Miss files and add bench script with apache bench + gnuplot

---
+++ tools/bench.sh
@@ -0,0 +1,2 @@
+ab -n 1000 -c 100 -g out.data http://127.0.0.1/glping/public/computers
+gnuplot gnuplottemplate
---
+++ tools/get_id1.sh
@@ -0,0 +1,1 @@
+curl -s -w \\n"%{time_connect}:%{time_starttransfer}:%{time_total}"\\n --request GET http://127.0.0.1/glping/public/computers/1
