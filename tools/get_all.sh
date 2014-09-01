b053da1b444d49929f6ca1282a74e2eba6540665
David Durieux <d.durieux@siprossii.com>
2014-09-01 16:31:27 +0200

    Update tools get

--- tools/get_all.sh
+++ tools/get_all.sh
@@ -1,1 +1,1 @@
-curl -s -w \\n"%{time_connect}:%{time_starttransfer}:%{time_total}"\\n --request GET http://127.0.0.1/glping/public/computers
+curl -s -w \\n"%{time_connect}:%{time_starttransfer}:%{time_total}"\\n --request GET http://127.0.0.1/api/computers
