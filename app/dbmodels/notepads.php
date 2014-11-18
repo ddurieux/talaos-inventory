b7e18a1bbca23a4fd013239174902b57392e23c2
MoYo <moyo@indepnet.net>
2014-11-18 16:22:49 +0100

    minor fix

--- app/dbmodels/notepads.php
+++ app/dbmodels/notepads.php
@@ -7,7 +7,7 @@ $table = array(
     'indexes'       => array(),
     'oldindexes'    => array(),
     'relationships' => array(),
-    'model'         => 'Infocom',
+    'model'         => 'Notepad',
     'menu'          => ''
 );
 $table['fields'] = array(
