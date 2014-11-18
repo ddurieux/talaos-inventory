93a65d082feec8b3fd275d9b32d1813df9977376
David Durieux <d.durieux@siprossii.com>
2014-11-18 11:10:30 +0100

    Add usertitle

---
+++ app/dbmodels/usertitles.php
@@ -0,0 +1,21 @@
+<?php
+
+$table = array(
+    'fields'        => array(),
+    'oldfields'     => array(),
+    'renamefields'  => array(),
+    'indexes'       => array(),
+    'oldindexes'    => array(),
+    'relationships' => array(),
+    'model'         => 'UserTitle',
+    'menu'          => 'Dropdown'
+);
+$table['fields'] = array(
+    'id'      => DBModels::type('increments',
+                                array('visible' => false)),
+    'name'    => DBModels::type('string'),
+    'comment' => DBModels::type('text'),
+);
+
+
+
---
+++ app/models/UserTitle.php
@@ -0,0 +1,12 @@
+<?php
+
+class UserTitle extends CommonGLPI {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'glpi_usertitles';
+
+}
