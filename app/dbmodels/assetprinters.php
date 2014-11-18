4568ee3288ee7511314618f3267328f40ebfd8f3
MoYo <moyo@indepnet.net>
2014-11-18 16:54:31 +0100

    printer capacity

---
+++ app/dbmodels/assetprinters.php
@@ -0,0 +1,34 @@
+<?php
+
+$table = array(
+    'fields'        => array(),
+    'oldfields'     => array(),
+    'renamefields'  => array(),
+    'indexes'       => array(),
+    'oldindexes'    => array(),
+    'relationships' => array(),
+    'model'         => 'AssetPrinter',
+    'menu'          => ''
+);
+$table['fields'] = array(
+    'id'                  => DBModels::type('increments', 
+                                            array('visible' => false)),
+    'asset_id'            => DBModels::type('integer', 
+                                            array('visible' => false)),
+    'printertype_id'      => DBModels::type('integer'),
+    'printermodel_id'     => DBModels::type('integer'),
+    'init_pages_counter'  => DBModels::type('integer'),
+    'last_pages_counter'  => DBModels::type('integer'),
+);
+
+$table['relationships'] = array(
+    'asset'        => array(
+        'type'  => 'belongsTo',
+        'item'  => 'Asset'),
+    'printertype'    => array(
+        'type'  => 'belongsTo',
+        'item'  => 'PrinterType'),
+    'printermodel'   => array(
+        'type'  => 'belongsTo',
+        'item'  => 'PrinterMoel'),
+);
---
+++ app/models/PrinterModel.php
@@ -0,0 +1,12 @@
+<?php
+
+class PrinterModel extends CommonGLPI {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'glpi_printermodels';
+
+}
