58ba68538938932a8466afae9cd29d91d6c028a7
David Durieux <d.durieux@siprossii.com>
2014-11-18 18:49:14 +0100

    Add state generation in seed script

--- app/seed.php
+++ app/seed.php
@@ -17,13 +17,24 @@ class Seed {
             $assettype->save();
         }
 
+        // State
+        $states = array('En panne', 'En service', 'Stock', 'Sortie de parc');
+        foreach ($states as $val) {
+            $state = new State;
+            $state->name = $val;
+            $state->save();
+        }
+
+
         // Asset
         for ($i=1; $i < 2000; $i++) {
-         $asset = new Asset;
-         $type = mt_rand(1, (count($assettypes) - 1));
-         $asset->name = $assettypes[$type].$i;
-         $asset->assettype_id = $type;
-         $asset->save();
+            $asset = new Asset;
+            $type = mt_rand(0, (count($assettypes) - 1));
+            $state = mt_rand(0, (count($states) - 1));
+            $asset->name = $assettypes[$type].$i;
+            $asset->assettype_id = $type;
+            $asset->state_id = $state;
+            $asset->save();
         }
     }
 }
