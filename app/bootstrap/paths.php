5315fc9acacddb5a7f72a072d00bc72b0c6dc0eb
Kevin Roy <kiniou@gmail.com>
2015-01-07 19:00:38 +0100

    fixed APP_PATH constant

--- app/bootstrap/paths.php
+++ app/bootstrap/paths.php
@@ -1,7 +1,7 @@
 <?php
 
-define( 'APP_PATH'      , __DIR__);
 define( 'ROOT_PATH'     , __DIR__ . '/../..');
+define( 'APP_PATH'      , ROOT_PATH . '/app');
 define( 'VAR_PATH'      , ROOT_PATH . '/var');
 define( 'LOG_PATH'      , ROOT_PATH . '/var/log');
 define( 'CONFIG_PATH'   , ROOT_PATH . '/config');
