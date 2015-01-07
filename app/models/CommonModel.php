ea8f4821f25f4a60bbf885a065e19b55b2b1d16a
David Durieux <d.durieux@siprossii.com>
2015-01-07 11:22:54 +0100

    Clean commonGLPI

--- app/models/CommonModel.php
+++ app/models/CommonModel.php
@@ -4,8 +4,12 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class CommonModel extends Eloquent {
 
+    static function getFkForModel($model) {
+        return snake_case($model).'_id';
+    }
+
     public function __call($name, $arguments) {
-        $ret = CommonGLPI::relationship($this, $name, $arguments);
+        $ret = $this->relationship($this, $name, $arguments);
         if (!empty($ret)) {
             return $ret;
         }
@@ -14,7 +18,52 @@ class CommonModel extends Eloquent {
 
 
     public function getFields($restrict='visible') {
-        return CommonGLPI::getFields($this, $restrict);
+        require __DIR__.'/../dbmodels/'.get_class($this).'.php';
+
+        $fields = array(
+            'data' => array(),
+            'meta' => array()
+        );
+
+        foreach ($table['fields'] as $field => $data) {
+            if (($restrict == 'visible'
+                    &&$data['visible'])
+                || $restrict == 'all') {
+                $fields['meta'][$field] = array(
+                   'type'  => $data['type'],
+                   'label' => $field
+                );
+                switch ($data['type']) {
+
+                    case 'datetime':
+                        $fields['data'][$field] = '0000-00-00 00:00:00';
+                        break;
+
+                    case 'integer':
+                        $fields['data'][$field] = 0;
+                        break;
+
+                    case 'boolean':
+                        $fields['data'][$field] = 0;
+                        break;
+
+                    default:
+                        $fields['data'][$field] = '';
+                        break;
+
+                }
+            }
+        }
+        foreach ($table['relationships'] as $name=>$data) {
+            if (isset($data['field'])) {
+                $fields['meta'][$field]['relationship'] = $data['item'];
+            } else {
+                $foreignKey = snake_case($name).'_id';
+
+                $fields['meta'][$foreignKey]['relationship'] = $data['item'];
+            }
+        }
+        return $fields;
     }
 
 
@@ -26,6 +75,101 @@ class CommonModel extends Eloquent {
 
 
     public function scopeRestrictentity($query, Entity $entity=null) {
-        return CommonGLPI::scopeRestrictentity($query, $entity);
+        if (is_null($entity)) {
+            return $query->with('entity');
+        }
+        $entityIds = $entity->getDescendantsAndSelf()->lists('id');
+        return $query->whereIn('entity_id', $entityIds);
     }
+
+
+    function relationship($item, $name, $arguments) {
+        require __DIR__.'/../dbmodels/'.get_class($item).'.php';
+
+        if (isset($table['relationships'][$name])) {
+            switch ($table['relationships'][$name]['type']) {
+
+                case 'belongsTo' :
+                    $field = NULL;
+                    if (isset($table['relationships'][$name]['field'])) {
+                        $field = $table['relationships'][$name]['field'];
+                    }
+                    return $item->belongsTo($table['relationships'][$name]['item'],
+                                            $field,
+                                            'id',
+                                            $name);
+                    break;
+
+                case 'belongsToMany' :
+                    $field1 = NULL;
+                    if (isset($table['relationships'][$name]['field1'])) {
+                        $field1 = $table['relationships'][$name]['field1'];
+                    }
+                    $field2 = NULL;
+                    if (isset($table['relationships'][$name]['field2'])) {
+                        $field2 = $table['relationships'][$name]['field2'];
+                    }
+                    $linktable = NULL;
+                    if (isset($table['relationships'][$name]['linktable'])) {
+                        $linktable = $table['relationships'][$name]['linktable'];
+                    }
+
+                    if (isset($table['relationships'][$name]['linkfields']) && is_array($table['relationships'][$name]['linkfields'])) {
+                        return $item->belongsToMany($table['relationships'][$name]['item'],
+                                                    $linktable,
+                                                    $field1,
+                                                    $field2)->withPivot($table['relationships'][$name]['linkfields']);
+
+                    } else {
+                        return($item->belongsToMany($table['relationships'][$name]['item'],
+                                                    $linktable,
+                                                    $field1,
+                                                    $field2));
+                    }
+                    break;
+
+                case 'morphTo' :
+                    return $item->morphTo($name,
+                                          'item_type',
+                                          'item_id');
+                    break;
+
+                case 'morphMany' :
+                    return $item->morphMany($table['relationships'][$name]['item'],
+                                            $name, 'item_type', 'item_id');
+                    break;
+
+                case 'morphToMany' :
+                    return $item->morphToMany($table['relationships'][$name]['item'],
+                                              'item',
+                                              $table['relationships'][$name]['table']);
+                    break;
+
+                case 'hasMany' :
+                    $field = NULL;
+                    if (isset($table['relationships'][$name]['field'])) {
+                        $field = $table['relationships'][$name]['field'];
+                    }
+
+                    return($item->hasMany($table['relationships'][$name]['item'],
+                                          $field,
+                                          'id',
+                                          $name));
+                    break;
+
+                case 'hasOne' :
+                    $field = NULL;
+                    if (isset($table['relationships'][$name]['field'])) {
+                        $field = $table['relationships'][$name]['field'];
+                    }
+
+                    return($item->hasMany($table['relationships'][$name]['item'],
+                                          $field,
+                                          'id'));
+                    break;
+
+            }
+        }
+    }
+
 }
