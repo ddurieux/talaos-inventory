0ee07a4996bdf7d78850433f9a2f7c0a6324345d
David Durieux <d.durieux@siprossii.com>
2015-01-15 17:46:15 +0100

    Update databases

---
+++ app/database/glpi_asset_type.py
@@ -0,0 +1,10 @@
+from app import db
+
+class DBAsset_Type(db.Model):
+    __tablename__ = 'glpi_asset_types'
+    __table_args__ = { 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255, None, True))
+    comment = db.Column(db.Text)
+    
+    
